2feec0bb0d2f3b2120ccd82079a46fe9d4553bdc
jennifertakagi <jennitakagi@gmail.com>
2021-01-07 11:36:06 -0300

    feat: add Upload component and style

---
+++ web/src/components/Upload/index.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from 'react';
+
+import Dropzone from 'react-dropzone';
+import { DropContainer, UploadMessage } from './styles';
+
+interface UploadProps {
+  onUpload: Function;
+}
+
+const Upload: React.FC<UploadProps> = ({ onUpload }: UploadProps) => {
+  function renderDragMessage(
+    isDragActive: boolean,
+    isDragRejest: boolean,
+  ): ReactNode {
+    if (!isDragActive) {
+      return (
+        <UploadMessage>Select or drag a file here.</UploadMessage>
+      );
+    }
+
+    if (isDragRejest) {
+      return <UploadMessage type="error">Not allowed file's extension</UploadMessage>;
+    }
+
+    return <UploadMessage type="success">Drop the file here</UploadMessage>;
+  }
+
+  return (
+    <>
+      <Dropzone accept=".csv, application/vnd.ms-excel, text/csv," onDropAccepted={(files) => onUpload(files)}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }): any => (
+          <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()} data-testid="upload" />
+            {renderDragMessage(isDragActive, isDragReject)}
+          </DropContainer>
+        )}
+      </Dropzone>
+    </>
+  );
+};
+
+export default Upload;
