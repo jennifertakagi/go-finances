cc9bb2ef09240baa7e3a611c8c7290fd0a1b297f
jennifertakagi <jennitakagi@gmail.com>
2021-01-07 11:31:52 -0300

    feat: add transactions routes

---
+++ backend/src/routes/transactions.routes.ts
@@ -0,0 +1,65 @@
+import { Router } from 'express';
+import multer from 'multer';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
+
+const transactionsRouter = Router();
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  const { category, title, type, value } = request.body;
+
+  if (!['income', 'outcome'].includes(type)) {
+    throw new AppError('The type must be: income or outcome! 😣');
+  }
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    category,
+    title,
+    type,
+    value,
+  });
+
+  return response.json(transaction);
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
+});
+
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+    const transactions = await importTransactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
+
+export default transactionsRouter;
